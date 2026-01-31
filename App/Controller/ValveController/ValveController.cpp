bb09b34c1f4394ba04e3a54e8c12fecb8634f689
Aliheal-Git <alican.ucar@metu.edu.tr>
2026-01-31 22:14:54 +0300

    ValveController: Add basic ValveController implementation

--- App/Controller/ValveController/ValveController.cpp
+++ App/Controller/ValveController/ValveController.cpp
@@ -1,4 +1,6 @@
 #include "ValveController.hpp"
+#include "ButtonCommon.hpp"
+#include "ValveCommon.hpp"
 
 using namespace App::Controller::ValveController;
 
@@ -6,3 +8,100 @@ ValveController::ValveController() {
     // Constructor implementation (if needed)
 }
 
+void ValveController::init(IValve* mainValvePtr, IValve* nozzleValvesArray[], uint8_t nozzleValveCnt) {
+    mainValve = mainValvePtr;
+    mainValve->setMode(ValveMode::AUTO);
+    nozzleValveCount = nozzleValveCnt > MAX_NOZZLE_VALVES ? MAX_NOZZLE_VALVES : nozzleValveCnt;
+    for (uint8_t i = 0; i < nozzleValveCount; ++i) {
+        nozzleValves[i] = nozzleValvesArray[i];
+    }
+    enabled = true;
+}
+
+void ValveController::enable() {
+    enabled = true;
+    if (activeNozzleValveCount > 0) {
+        mainValve->open(ValveMode::AUTO);
+    }
+}
+
+void ValveController::disable() {
+    enabled = false;
+    mainValve->close(ValveMode::AUTO);
+}
+
+void ValveController::setMode(ValveControllerMode newMode) {
+    mode = newMode;
+    if (mode == ValveControllerMode::MANUAL) {
+        mainValve->setMode(ValveMode::MANUAL);
+        for (uint8_t i = 0; i < nozzleValveCount; ++i) {
+            nozzleValves[i]->setMode(ValveMode::MANUAL);
+        }
+    } else {
+        for (uint8_t i = 0; i < nozzleValveCount; ++i) {
+            nozzleValves[i]->setMode(ValveMode::AUTO);
+        }
+    }
+}
+
+ValveControllerTaskResult ValveController::task() {
+    if (mode == ValveControllerMode::GPS) {
+        for (uint8_t i = 0; i < nozzleValveCount; ++i) {
+            if (gps->getValveState(i)) {
+                if (nozzleValves[i]->getStatus() == ValveState::CLOSED) {
+                    if (activeNozzleValveCount < nozzleValveCount) {
+                        activeNozzleValveCount++;
+                    }
+                    if (activeNozzleValveCount == 1) {
+                        mainValve->open(ValveMode::AUTO);
+                    }
+                    nozzleValves[i]->open(ValveMode::AUTO);
+                }
+            } else if (nozzleValves[i]->getStatus() == ValveState::OPEN) {
+                nozzleValves[i]->close(ValveMode::AUTO);
+                if (activeNozzleValveCount > 0) {
+                    activeNozzleValveCount--;
+                    if (activeNozzleValveCount == 0) {
+                        mainValve->close(ValveMode::AUTO);
+                    }
+                }
+            }
+        }
+    }
+
+    return {true, 100}; // Executed, check again in 500 ms
+}
+
+void ValveController::onButtonEvent(const ButtonQueueEvent& queueEvent) {
+    if (queueEvent.buttonType == ButtonType::MAIN) {
+        if (queueEvent.eventType == ButtonEventType::PRESSED) {
+            if (activeNozzleValveCount > 0) {
+                mainValve->open(ValveMode::AUTO);
+            } 
+        } else if (queueEvent.eventType == ButtonEventType::RELEASED) {
+            mainValve->close(ValveMode::AUTO);
+        }
+        return;
+    }
+    for (uint8_t i = static_cast<uint8_t>(ButtonType::VALVE1); i <= static_cast<uint8_t>(ButtonType::VALVE1) + nozzleValveCount - 1; ++i) {
+        if (queueEvent.buttonType == static_cast<ButtonType>(i)) {
+            if (queueEvent.eventType == ButtonEventType::PRESSED) {
+                nozzleValves[i - static_cast<uint8_t>(ButtonType::VALVE1)]->open(ValveMode::MANUAL);
+                activeNozzleValveCount++;
+                if (activeNozzleValveCount == 1) {
+                    mainValve->open(ValveMode::AUTO);
+                }
+                return;
+            } else if (queueEvent.eventType == ButtonEventType::RELEASED) {
+                nozzleValves[i - static_cast<uint8_t>(ButtonType::VALVE1)]->close(ValveMode::MANUAL);
+                if (activeNozzleValveCount > 0) {
+                    activeNozzleValveCount--;
+                    if (activeNozzleValveCount == 0) {
+                        mainValve->close(ValveMode::AUTO);
+                    }
+                }
+                return;
+            }
+        }
+    }
+}
--- App/Controller/ValveController/ValveController.hpp
+++ App/Controller/ValveController/ValveController.hpp
@@ -5,31 +5,46 @@
 #ifndef APP_CONTROLLER_VALVECONTROLLER_VALVECONTROLLER_HPP_
 #define APP_CONTROLLER_VALVECONTROLLER_VALVECONTROLLER_HPP_
 
+#include "ButtonCommon.hpp"
+#include "ButtonService.hpp"
 #include "ControllerCommon.hpp"
+#include "IButton.hpp"
 #include "IValve.hpp"
 #include "ValveCommon.hpp"
+#include "IGPS.hpp"
+
+#define MAX_NOZZLE_VALVES 8
 
 using namespace App::Domain;
 using namespace App::Interfaces;
 
 namespace App::Controller::ValveController {
-
-    class ValveController {
+    struct ValveControllerTaskResult {
+        bool executed;
+        uint32_t nextTickDelayMs;
+    };
+    class ValveController : public IButtonEventListener{
         public:
             ValveController();
-            void init();
+            void init(IValve* mainValvePtr, IValve* nozzleValvesArray[], uint8_t nozzleValveCnt);
             void enable();
             void disable();
 
+            void setValveState(uint8_t valveIndex, ValveState state, ValveMode mode);
             void setMode(ValveControllerMode newMode);
             ValveState getState(uint8_t valveIndex) const;
+            ValveControllerTaskResult task();
+
+            void onButtonEvent(const ButtonQueueEvent& queueEvent) override;
 
         private:
             bool enabled = false;
             ValveControllerMode mode = ValveControllerMode::MANUAL;
             IValve* mainValve;
-            IValve* nozzleValves[8];
+            IValve* nozzleValves[MAX_NOZZLE_VALVES];
             uint8_t nozzleValveCount = 0;
+            uint8_t activeNozzleValveCount = 0;
+            IGPS* gps;
     };
 
 } // namespace App::Controller::ValveController
