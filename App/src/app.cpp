19b7d6a2e4ba87a0de878ad6488253a99a3613d2
Aliheal-Git <alican.ucar@metu.edu.tr>
2026-01-31 14:21:42 +0300

    Refactor: Remove unused includes and objects from app.cpp

--- App/src/app.cpp
+++ App/src/app.cpp
@@ -1,84 +1,32 @@
-/*
- * app.cpp
- *
- *  Created on: Aug 24, 2025
- *      Author: alica
- */
 #include "app.h"
-
-#include "Page.hpp"
-#include "cmsis_os.h"
-#include "gpio.h"
 #include "tim.h"
-#include "main.h"
 
-#include "Window.hpp"
 #include "Flowmeter.hpp"
-#include "Controller.hpp"
-#include "micros.h"
-#include "Valve.hpp"
-#include "Settings.hpp"
-
 #include "Button.h"
+#include "ButtonService.hpp"
 
-volatile uint32_t overFlowCounter = 0;
-
-Flowmeter flowmeter(637);
-Controller controller;
-Settings settings;
+volatile uint8_t flowmeterPulseCounter = 0;
+namespace App {
+	Drivers::Flowmeter flowmeter(450, 20000.0f, 0.1f, 0.01f, 1.0f, 0.01f);
 	
-ProportionalValve propValve(&htim1, TIM_CHANNEL_1, &htim1, TIM_CHANNEL_2);
-Valve mainValve(VALF8_ON_GPIO_Port, VALF8_ON_Pin, VALF8_OFF_GPIO_Port, VALF8_OFF_Pin);
-Valve Valves[5] = {
-	Valve(VALF1_ON_GPIO_Port, VALF1_ON_Pin, VALF1_OFF_GPIO_Port, VALF1_OFF_Pin),
-	Valve(VALF2_ON_GPIO_Port, VALF2_ON_Pin, VALF2_OFF_GPIO_Port, VALF2_OFF_Pin),
-	Valve(VALF3_ON_GPIO_Port, VALF3_ON_Pin, VALF3_OFF_GPIO_Port, VALF3_OFF_Pin),
-	Valve(VALF4_ON_GPIO_Port, VALF4_ON_Pin, VALF4_OFF_GPIO_Port, VALF4_OFF_Pin),
-	Valve(VALF5_ON_GPIO_Port, VALF5_ON_Pin, VALF5_OFF_GPIO_Port, VALF5_OFF_Pin)
-};
 
-Window mainWindow;
-std::unique_ptr<WelcomePage> welcomePage;
-std::unique_ptr<MainPage> mainPage;
-std::unique_ptr<SettingsPage> settingsPage;
-std::unique_ptr<ProgramsPage> programsPage;
-std::unique_ptr<InformationPage> informationPage;
+	// Buttons
+	Services::ButtonService buttonService;
+	Drivers::Button mainButton(MAIN, 0xFFFFFFFFFFFF, 0, false, BUTTON_MAIN_GPIO_Port, BUTTON_MAIN_Pin, &buttonService);
+}
+
 
 void app_init(){
-	startMicros();
 	HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_2);
 	HAL_TIM_Base_Start_IT(&htim3);
-
-	settings.disableWPFlash();
-	settings.readSettingsFlash();
-	
-	controller.setTargetDosage(0);
-
-	controller.init();
-	propValve.init();
-
-
-	mainWindow.initiliaze();
-	welcomePage = std::make_unique<WelcomePage>(&mainWindow);
-	mainPage = std::make_unique<MainPage>(&mainWindow);
-	settingsPage = std::make_unique<SettingsPage>(&mainWindow);
-	programsPage = std::make_unique<ProgramsPage>(&mainWindow);
-	informationPage = std::make_unique<InformationPage>(&mainWindow);
-	mainWindow.addPage(std::move(welcomePage));
-	mainWindow.addPage(std::move(mainPage));
-	mainWindow.addPage(std::move(settingsPage));
-	mainWindow.addPage(std::move(programsPage));
-	mainWindow.addPage(std::move(informationPage));
-	mainWindow.showPage(0);
-	xTaskNotifyGive(*mainWindow.getTaskHandle());
 }
 
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim){
     if (htim->Instance == TIM3 && htim->Channel == HAL_TIM_ACTIVE_CHANNEL_2) {
-    	temp_counter++;
-    	if (temp_counter == 10){
-    		flowmeter.calculateFrequency(HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_2));
-    		temp_counter = 0;
+    	flowmeterPulseCounter++;
+    	if (flowmeterPulseCounter >= PUSLECOUNTPERDURATION){
+    		App::flowmeter.calculateFrequency(HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_2));
+    		flowmeterPulseCounter = 0;
     	}
     }
 }
@@ -90,6 +38,6 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
     HAL_IncTick();
   }
   if (htim->Instance == TIM3){
-	  flowmeter.increaseOverFlow();
+	  App::flowmeter.onTimerOverflow();
   }
 }
